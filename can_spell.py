8c47bb1a2a0df93cf5bafe6e73ae9a6767b8385e
I. J. Kennedy <jack@realmode.com>
2022-06-02 06:28:48 -0500

    Add comment to `can_spell`
    See [#1][1].
    
    [1]: https://github.com/EvanHahn/scrabbler/pull/1

--- can_spell.py
+++ can_spell.py
@@ -1,4 +1,6 @@
 def can_spell(letters, word):
+    # reverse sort to get the blanks ('?') to the end of letters string,
+    # so that the greedy algorithm works
     letters = sorted(letters, reverse=True)
     word = list(word)
 
